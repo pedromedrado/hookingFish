567f682b4e373e68da97892aab653ba8d28f029d
Pedro Medrado <pedromedrado603@gmail.com>
2023-03-14 22:56:13 -0300

    firstcommit

---
+++ app/src/main/java/com/example/hookingfish/AddDialog.kt
@@ -0,0 +1,23 @@
+package com.example.hookingfish
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class AddDialog : DialogFragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog,container)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/hookingfish/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.hookingfish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+
+class MainActivity2 : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+
+    }
+}
